b904cec8154e82bef7807bded5cbc74a6f130a0a
ruben_ohoifpj <rubenzj98@gmail.com>
2019-02-24 18:07:00 +0100

    JUnit pruebas

--- app/src/test/java/spaceInvaders/ChangeColoursUnitTest.java
+++ app/src/test/java/spaceInvaders/ChangeColoursUnitTest.java
@@ -11,18 +11,35 @@ import static org.junit.Assert.*;
 
 public class ChangeColoursUnitTest {
     @Test
-    public void changeColoursTest(){
+    public void changeColoursAlienTest(){
         MayorActivity activity = new MayorActivity();
 
         /*Intent mayor = new Intent(new MainActivity(), activity.getClass());
         startActivity(mayor);*/
 
-        String before= activity.getSpaceInvadersJuego().getAliens().get(1).getColor();
+        String beforeAlien= activity.getSpaceInvadersJuego().getAliens().get(1).getColor();
         activity.getSpaceInvadersJuego().getAliens().get(1).cambiarColorRandom();
-        String after= activity.getSpaceInvadersJuego().getAliens().get(1).getColor();
-        boolean colours= areSame(before,after);
+        String afterAlien= activity.getSpaceInvadersJuego().getAliens().get(1).getColor();
+        boolean coloursAlien= areSame(beforeAlien,afterAlien);
 
-        assertTrue(colours);
+
+        assertTrue(coloursAlien);
+
+    }
+
+    @Test
+    public void changeColoursNaveTest(){
+        MayorActivity activity = new MayorActivity();
+
+        /*Intent mayor = new Intent(new MainActivity(), activity.getClass());
+        startActivity(mayor);*/
+
+        String beforeNave= activity.getSpaceInvadersJuego().getNave().getColor();
+        activity.getSpaceInvadersJuego().getNave().colorRandomNave();
+        String afterNave= activity.getSpaceInvadersJuego().getNave().getColor();
+        boolean coloursNave= areSame(beforeNave,afterNave);
+
+        assertTrue(coloursNave);
 
     }
 
