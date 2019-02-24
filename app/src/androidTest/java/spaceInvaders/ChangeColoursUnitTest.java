4cba8b5091da8f3889ce6ce716ab3bd829ee36cc
ruben_ohoifpj <rubenzj98@gmail.com>
2019-02-25 00:23:45 +0100

    Cambios en las pruebas

--- app/src/androidTest/java/spaceInvaders/ChangeColoursUnitTest.java
+++ app/src/androidTest/java/spaceInvaders/ChangeColoursUnitTest.java
@@ -18,13 +18,15 @@ public class ChangeColoursUnitTest extends AppCompatActivity {
 
     private String before;
     private String after;
+    private String beforeNave;
+    private String afterNave;
     private boolean colours;
+    private boolean coloursNave;
     private MayorActivity activity;
 
 
     @Before
     public void Iniciar(){
-
         activity = new MayorActivity();
         Intent mayor = new Intent(activity, activity.getClass());
         startActivity(mayor);
@@ -36,7 +38,6 @@ public class ChangeColoursUnitTest extends AppCompatActivity {
         before= activity.getSpaceInvadersJuego().getAliens().get(0).getColor();
         activity.getSpaceInvadersJuego().getAliens().get(0).cambiarColorRandom();
         after= activity.getSpaceInvadersJuego().getAliens().get(0).getColor();
-        finish();
     }
 
     @Test
@@ -44,6 +45,25 @@ public class ChangeColoursUnitTest extends AppCompatActivity {
         colours= areSame(before,after);
         assertTrue(colours);
     }
+
+
+
+
+    @Test
+    public void actualizarNave() {
+        assertNotNull(activity.getSpaceInvadersJuego().getNave());
+        beforeNave= activity.getSpaceInvadersJuego().getNave().getColor();
+        activity.getSpaceInvadersJuego().getNave().colorRandomNave();
+        afterNave= activity.getSpaceInvadersJuego().getNave().getColor();
+    }
+
+    @Test
+    public void actualizarColorNave() {
+        coloursNave= areSame(beforeNave,afterNave);
+        assertTrue(coloursNave);
+    }
+
+
 
     /*@Test
     public void changeColoursAlienTest(){
@@ -65,7 +85,7 @@ public class ChangeColoursUnitTest extends AppCompatActivity {
 
     }*/
 
-    @Test
+    /*@Test
     public void changeColoursNaveTest(){
         activity = new MayorActivity();
         colours=false;
@@ -82,7 +102,7 @@ public class ChangeColoursUnitTest extends AppCompatActivity {
 
         assertTrue(colours);
 
-    }
+    }*/
 
     public boolean areSame(String colour1, String colour2){
         return !colour1.equals(colour2);
